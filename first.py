ccf59afb627a1c8a3b4e38282d4ddaf24a88bb42
SHIVANI SEHGAL <147038954+Shivani77089@users.noreply.github.com>
2025-07-18 18:00:40 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from datetime import time
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Flask-Monitoring-Dashboard tutorial'
+
+
+@app.route('/endpoint1')
+def endpoint1():
+    time.sleep(0.20)
+    return 'Endpoint1', 400
+
+
+@app.route('/endpoint2')
+def endpoint2():
+    time.sleep(5)
+    return 'Endpoint2'
+
+app.run(debug=True)
